75e00a9bf688e64c7c9b69d2effdd94ec5f060fd
damla yaşar <66484788+damla-debug@users.noreply.github.com>
2021-09-26 18:53:44 +0300

    Update index.js

--- frontend/view/index.js
+++ frontend/view/index.js
@@ -35,13 +35,61 @@ function addTodo(e){
      todoElement.appendChild(document.createTextNode(todo)); //üretilen todo buraya yazılacak
      todoElement.appendChild(link);
      listItemBody.appendChild(todoElement);
+      getInput.value=" ";
   }
 
 
-   function addtoStorage (todo){
+    function addtoStorage (todos){
+       let todo=getTodotothestorage();
 
+       todo.push(todos);
+       localStorage.setItem("todos", JSON.stringify(todo))
     
    }
+
+   function getTodotothestorage(){
+      let todos; 
+
+      if(localStorage.getItem("todos")===null){
+        todos= []
+      }else{
+        todos=JSON.parse(localStorage.getItem("todos"))
+      }
+
+
+      return todos;
+   }
+
+   function loadTodosFromIU(){
+     let todo=getTodotothestorage();
+     todo.forEach(function (to){
+       addTodoUI(to);
+     })
+   }
+
+
+   function deleteFromStorage(todos){
+      let todo=getTodotothestorage();
+
+      todo.forEach(function(to,index){
+        if(to===todos){
+          todo.splice(index,1);
+        }
+      })
+      localStorage.setItem("todos", JSON.stringify(todo));
+   }
+
+   function deleteFromUI(e){
+     
+   if(e.target.className==="delete-item"){
+     getInput.value.style.textDecoration="overline"
+    e.target.parentElement.parentElement.remove();
+    
+     deleteFromStorage(e.target.parentElement.parentElement.textContent);
+     console.log("başarı ile silindi")
+   }
+   }
+
 
 
 
